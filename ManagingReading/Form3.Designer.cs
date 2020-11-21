906418ff6b658365fc347a1500adecf5608ce8de
salem alm <i.melas@hotmail.com>
2020-11-22 02:09:04 +0300

    update

--- ManagingReading/Form3.Designer.cs
+++ ManagingReading/Form3.Designer.cs
@@ -51,7 +51,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 11);
+            this.label2.Location = new System.Drawing.Point(20, 10);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(156, 20);
             this.label2.TabIndex = 1;
